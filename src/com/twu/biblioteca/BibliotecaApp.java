81cd6d8fcf416cd8bdeaaccceadd6981d83f622c
Matheus Bernardes <bernardes.matheus@outlook.com>
2020-01-24 19:34:21 -0300

    [Matheus] [1.1] [1.2] Made welcome message appear at start and a list of available books appear right after

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,21 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    public static String lastMessage;
+    private static final String WELCOME_MESSAGE = "Welcome to Biblioteca! Your one-stop-shop for great book titles in Bangalore!\n";
+    private static String availableBooks = "Orope: The White Snake - Guenevere Lee\nThe Great Gatsby - F. Scott Fitzgerald\nBoulevard Dreams - E. Ryan Janz";
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp.showWelcomeMessage();
+    }
+
+    public static void showWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+        lastMessage = WELCOME_MESSAGE;
+    }
+
+    public static void displayAllBooks() {
+        System.out.println(availableBooks);
+        lastMessage = availableBooks;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,28 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private BibliotecaApp app;
+
+    @Before
+    public void init() {
+        app = new BibliotecaApp();
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldDisplayWelcomeMessage() {
+        BibliotecaApp.showWelcomeMessage();
+        assertEquals("Welcome to Biblioteca! Your one-stop-shop for great book titles in Bangalore!\n", BibliotecaApp.lastMessage);
+    }
+
+    @Test
+    public void shouldShowAListOfAllLibraryBooks(){
+        BibliotecaApp.displayAllBooks();
+        assertEquals("Orope: The White Snake - Guenevere Lee\nThe Great Gatsby - F. Scott Fitzgerald\nBoulevard Dreams - E. Ryan Janz", BibliotecaApp.lastMessage);
     }
 }
