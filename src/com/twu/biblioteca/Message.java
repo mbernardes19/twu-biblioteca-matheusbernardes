0b67ff3675d707d3b1e4a763c9e47cf9d9a84985
Matheus Bernardes <bernardes.matheus@outlook.com>
2020-02-04 21:40:07 -0300

    [Matheus] Refactored messages to Message class

---
+++ src/com/twu/biblioteca/Message.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+public class Message {
+    public static final String WELCOME_MESSAGE = "Welcome to Biblioteca! Your one-stop-shop for great book titles in Bangalore!";
+    public static final String ERROR_MESSAGE = "Please select a valid option!";
+    public static final String BOOK_CHECKOUT_SUCCESS_MESSAGE = "Thank you! Enjoy the book\n";
+    public static final String BOOK_CHECKOUT_ERROR_MESSAGE = "Sorry, that book is not available\n";
+    public static final String BOOK_RETURN_SUCCESS_MESSAGE = "Thank you for returning the book\n";
+    public static final String BOOK_RETURN_ERROR_MESSAGE = "That is not a valid book to return\n";
+}
